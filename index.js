742c7430c82341746b9ea58057f0ee0b50aeb85b
sandra838 <109079326+sandra838@users.noreply.github.com>
2022-11-04 22:34:43 +0100

    started function 2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// 1. 
 const function1 =(num1, num2) => {
   return num1 ===50 || num2 === 50 || num1 + num2 === 50;
 
 }
+
+
 console.log(function1(50, 10))
 console.log(function1(20, 30))
 console.log(function1(30, 10))
+
+// 2.
+
